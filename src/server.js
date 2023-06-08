5a563b7d5c648d6fe3991f5823aae5ca3408f1af
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2023-06-08 16:00:56 +0300

    jsdom

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const https = require('node:https');
 const app = express()
 const port = 3000
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const SITE = 'https://scrapeme.live/shop/';
 const test = 'https://jsonplaceholder.typicode.com/users'
@@ -16,7 +18,16 @@ app.get('/', (req, res) => {
         });
 
         res.on('end', () => {
-            console.log(data)
+            // console.log(data)
+            const document = new JSDOM(data).window.document;
+            let elements = document.getElementsByTagName('A');
+            const t = Array.from(elements)
+                .map(el => el.getAttribute('href'))
+                .filter(href => typeof href === 'string')
+                .map(href => href.trim())
+                .filter(Boolean);
+
+            console.log(t)
         })
     }).on('error', err => {
         console.log('Error: ', err.message);
